0472a14736ce4ef6c23025de1d739483ee57d5a8
KTNguyener <30875266+KTNguyener@users.noreply.github.com>
2017-08-14 09:11:23 -1000

    Updated js file to ask 3 questions and display all

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 function sayHello() {
   //I create a function called sayHello//
-  var userName = prompt("What is your name?");
-  //I create a variable called userName and ask the user something//
-  window.alert("Hello " + userName);
+  var userFirst= prompt("What is your first name?");
+  //I create a variable called userFirst to ask the user their first name//
+  var userNick= prompt("What is your nick name?");
+  //I create a variable called userNick to ask the user their nick name//
+  var userLast= prompt("What is your last name?");
+  //I create a variable called userLast to ask the user their last name//
+  window.alert("Hello " + userFirst + " " + userNick + " "+ userLast);
   //I create a popup window telling the userName hello//
 }
